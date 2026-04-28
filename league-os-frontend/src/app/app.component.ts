735d46c7b5fbd1a7111e982922a9fd266ef5ff17
dandreev <Azazello1992Kot!>
2026-04-28 20:43:49 +0300

    рефакторинг footer.component

--- league-os-frontend/src/app/app.component.ts
+++ league-os-frontend/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './widgets/header/header.component';
 import { HomeComponent } from './features/home/home.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
+import { FooterComponent } from './widgets/footer/footer.component';
 import { BreadcrumbsComponent } from './shared/components/breadcrumbs/breadcrumbs.component';
 import { MainComponent } from './features/main/main.component';
 
