259e942c7e1ab48674a3003cc8ee7f1eb88fbb52
dandreev <Azazello1992Kot!>
2026-05-28 13:12:50 +0300

    локальная подгрузка шрифтов. Оптимизация под поисковые машины

--- league-os-frontend/src/environments/environment.ts
+++ league-os-frontend/src/environments/environment.ts
@@ -1,4 +1,8 @@
+// export const environment = {
+//     production: false,
+//     apiUrl: '/api',
+// };
+
 export const environment = {
-    production: false,
-    apiUrl: '/api',
+    apiUrl: 'http://localhost:3000',
 };
