b18e02af20c445e552593f1ed7133cf87cf3e976
dandreev <Azazello1992Kot!>
2026-06-25 13:52:49 +0300

    поправил запрос протокола

--- league-os-backend/src/modules/matches/matches.module.ts
+++ league-os-backend/src/modules/matches/matches.module.ts
@@ -3,9 +3,17 @@ import { MatchesController } from './matches.controller';
 import { MatchesService } from './matches.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MatchEntity } from './entities/match.entity';
+import { MatchRosterEntity } from '../match-rosters/entities/match-roster.entity';
+import { MatchRosterPlayerEntity } from '../match-rosters/entities/match-roster-player.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([MatchEntity])],
+  imports: [
+    TypeOrmModule.forFeature([
+      MatchEntity,
+      MatchRosterEntity,
+      MatchRosterPlayerEntity,
+    ]),
+  ],
   controllers: [MatchesController],
   providers: [MatchesService],
   exports: [MatchesService],
