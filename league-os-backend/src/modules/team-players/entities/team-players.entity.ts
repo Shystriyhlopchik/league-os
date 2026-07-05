7c20c30520c89d8360cfc728460309bdcc8b0157
dandreev <Azazello1992Kot!>
2026-07-05 17:46:09 +0300

    применил преттир

--- league-os-backend/src/modules/team-players/entities/team-players.entity.ts
+++ league-os-backend/src/modules/team-players/entities/team-players.entity.ts
@@ -1,50 +1,45 @@
-import {
-    Column,
-    Entity,
-    JoinColumn,
-    ManyToOne,
-} from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
 
 import { TeamEntity } from '../../teams/entities/team.entity';
 import { PlayerEntity } from '../../players/entities/player.entity';
 import { PlayerPosition } from '../../players/enums/player-position.enum';
-import {BaseEntity} from "../../../common/base/base.entity";
+import { BaseEntity } from '../../../common/base/base.entity';
 
 @Entity('team_players')
 export class TeamPlayerEntity extends BaseEntity {
-    @Column({ name: 'team_id' })
-    teamId: number;
+  @Column({ name: 'team_id' })
+  teamId: number;
 
-    @Column({ name: 'player_id' })
-    playerId: number;
+  @Column({ name: 'player_id' })
+  playerId: number;
 
-    @ManyToOne(() => PlayerEntity, { nullable: false })
-    @JoinColumn({ name: 'player_id' })
-    player: PlayerEntity;
+  @ManyToOne(() => PlayerEntity, { nullable: false })
+  @JoinColumn({ name: 'player_id' })
+  player: PlayerEntity;
 
-    @Column({ nullable: true })
-    shirtNumber?: number;
+  @Column({ nullable: true })
+  shirtNumber?: number;
 
-    @Column({
-        type: 'enum',
-        enum: PlayerPosition,
-        nullable: true,
-    })
-    position?: PlayerPosition;
+  @Column({
+    type: 'enum',
+    enum: PlayerPosition,
+    nullable: true,
+  })
+  position?: PlayerPosition;
 
-    @Column({ default: false })
-    isCaptain: boolean;
+  @Column({ default: false })
+  isCaptain: boolean;
 
-    @Column({ default: true })
-    isActive: boolean;
+  @Column({ default: true })
+  isActive: boolean;
 
-    @Column({ type: 'date', nullable: true })
-    joinedAt?: string;
+  @Column({ type: 'date', nullable: true })
+  joinedAt?: string;
 
-    @Column({ type: 'date', nullable: true })
-    leftAt?: string;
+  @Column({ type: 'date', nullable: true })
+  leftAt?: string;
 
-    @ManyToOne(() => TeamEntity, { nullable: false })
-    @JoinColumn({ name: 'team_id' })
-    team: TeamEntity;
+  @ManyToOne(() => TeamEntity, { nullable: false })
+  @JoinColumn({ name: 'team_id' })
+  team: TeamEntity;
 }
